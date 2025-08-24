136acebf7adaef2602a7b4871eee1227b47793aa
i-am-ashwin <aswin.harikumar@yahoo.com>
2025-08-25 00:06:46 +0200

    feat: init layout and add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body >
+      <body className="antialiased">
       <div className="min-h-screen bg-pink-100">
         <Header />
         <main className="max-w-6xl mx-auto px-4 py-4">
